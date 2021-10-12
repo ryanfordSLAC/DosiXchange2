351d464d1b01ae683b829f9e0a4acb5a75000e8b
Matt Lintlop <mattl@slac.stanford.edu>
2021-10-11 17:35:55 -0700

    saveCache() run asynchronously on a background thread

--- LocationApp/LocationApp/Location Cache/LocationCache.swift
+++ LocationApp/LocationApp/Location Cache/LocationCache.swift
@@ -62,11 +62,16 @@ class LocationCache: Codable {
     // Save the dosimeter CloudKit records from disk.
     // Throws a LocationCacheError is a required record field is nil.
      func savedCache() throws {
-        print("Saving the Locations Cache")
-        let cacheData = try JSONEncoder().encode(self)
-        let cacheFileURL = URL(fileURLWithPath: cacheFilePath())
-        let file = try cacheData.write(to: cacheFileURL)
-        print("SUCCESS! Saved the Locations Cache")}
+         DispatchQueue.global().async {
+             print("Saving the Locations Cache")
+             let cacheData = try JSONEncoder().encode(self)
+             let cacheFileURL = URL(fileURLWithPath: cacheFilePath())
+             let file = try cacheData.write(to: cacheFileURL)
+             print("SUCCESS! Saved the Locations Cache")
+             
+         }
+    }
+
     
     // Path to the locations cache file
     func cacheFilePath() -> String {
